c0426556213492515d0d7ea8c6eea69447569428
juniorfocusnik <dk@smeola.com>
2024-08-16 12:04:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function createSprinkles() {
+  const sprinklesContainer = document.getElementById ("container");
+  for (let i = 0; i < 400; i++) {
+    const sprinkle = document.createElement("div");
+    sprinkle.className = "sprinkle";
+    sprinkle.style.backgroundColor = "rgb(" + 
+ Math.floor(Math.random() * 254 + 1) + ", " + Math.floor(Math.random() * 254 + 1) + ", " + Math.floor(Math.random() * 254 + 1) + ")";
+    sprinkle.style.left = Math.random() * window.innerWidth - 30 + "px";
+    sprinkle.style.animationDuration = (Math.random() * 4 + 2) + "s";
+    sprinklesContainer.appendChild(sprinkle);
+  }
+}
+
+createSprinkles();
