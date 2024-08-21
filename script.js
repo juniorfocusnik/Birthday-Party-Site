e16e79f931036d37633c69cdef4c0a6e49975d11
juniorfocusnik <dk@smeola.com>
2024-08-21 11:49:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function createSprinkles() {
     sprinkle.className = "sprinkle";
     sprinkle.style.backgroundColor = "rgb(" + 
  Math.floor(Math.random() * 254 + 1) + ", " + Math.floor(Math.random() * 254 + 1) + ", " + Math.floor(Math.random() * 254 + 1) + ")";
-    sprinkle.style.left = Math.random() * window.innerWidth * 2 + "px";
+    sprinkle.style.left = Math.random() * window.innerWidth + "px";
     sprinkle.style.animationDuration = (Math.random() * 4 + 2) + "s";
     sprinklesContainer.appendChild(sprinkle);
   }
