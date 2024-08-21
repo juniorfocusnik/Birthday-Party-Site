556458647670a7bec5403a99d046a9e0ffa3b57e
juniorfocusnik <dk@smeola.com>
2024-08-21 11:41:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function createSprinkles() {
   const sprinklesContainer = document.getElementById ("container");
-  for (let i = 0; i < 400; i++) {
+  for (let i = 0; i < 800; i++) {
     const sprinkle = document.createElement("div");
     sprinkle.className = "sprinkle";
     sprinkle.style.backgroundColor = "rgb(" + 
  Math.floor(Math.random() * 254 + 1) + ", " + Math.floor(Math.random() * 254 + 1) + ", " + Math.floor(Math.random() * 254 + 1) + ")";
-    sprinkle.style.left = Math.random() * window.innerWidth - 30 + "px";
+    sprinkle.style.left = Math.random() * window.innerWidth * 2 + "px";
     sprinkle.style.animationDuration = (Math.random() * 4 + 2) + "s";
     sprinklesContainer.appendChild(sprinkle);
   }
